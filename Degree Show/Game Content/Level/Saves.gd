3eced5ea98bb69da5fb1df9e847ad0e0659c44e0
JarodShorrockGames <jarod.shorrock@gmail.com>
2024-05-01 09:53:14 +0100

    Whitebox Updated with Plans for Further Concepts, UI Elements in Place

---
+++ Degree Show/Game Content/Level/Saves.gd
@@ -0,0 +1,12 @@
+extends Label3D
+
+@onready var label_3d_4 = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$".".text = "1"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
