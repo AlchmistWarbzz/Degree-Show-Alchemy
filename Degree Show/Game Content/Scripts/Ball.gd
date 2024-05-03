e775c461fda6dcf09305889428ba19808d335ea5
AlchmistWarbzz <ALchemistwarbzz@gmail.com>
2024-05-03 21:03:40 +0100

    Random Z angle

--- Degree Show/Game Content/Scripts/Ball.gd
+++ Degree Show/Game Content/Scripts/Ball.gd
@@ -1,12 +1,14 @@
 extends RigidBody3D
 
-
 func kick(target: Vector3) -> void:
 	set_linear_velocity(Vector3.ZERO)
 	set_angular_velocity(Vector3.ZERO)
+	# Add a random range to the Z component of the target vector
+	var random_angle = randf_range(-PI/4, PI/4) # Adjust the range as needed
+	target.z += random_angle
 	look_at(target)
 	print("Works")
-	apply_central_impulse(get_global_transform().basis.z * -6)
+	apply_central_impulse(get_global_transform().basis.z * -16)
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
