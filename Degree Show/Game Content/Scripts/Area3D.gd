14688add257aca65f5c48f8c7d1a0e4d73852f47
AlchmistWarbzz <146832964+AlchmistWarbzz@users.noreply.github.com>
2024-05-08 17:04:21 +0100

    IT just works

--- Degree Show/Game Content/Scripts/Area3D.gd
+++ Degree Show/Game Content/Scripts/Area3D.gd
@@ -12,14 +12,10 @@ var ball
 		#var angle = randf_range(min_angle, max_angle)
 		#ball.set_linear_velocity(-ball.get_linear_velocity() + angle)
 func _on_body_entered(body):
-	print("has worked")
+	#print("has worked")
 	if body is RigidBody3D:
 		ball = body as RigidBody3D
 		#Calculate the direction from the ball to the center of the Area3D
-		var direction = global_transform.origin - ball.global_transform.origin
-		# Reflect the ball's velocity around the normal of the collision surface
-		var reflection = direction.normalized().reflect(ball.linear_velocity.normalized())
-		# Apply the reflected velocity to the ball
-		ball.set_linear_velocity(reflection * ball.linear_velocity.length()) 
+
 
 
--- Degree Show/Game Content/Scripts/Ball.gd
+++ Degree Show/Game Content/Scripts/Ball.gd
@@ -1,28 +1,7 @@
 extends RigidBody3D
 var velocity
 var spawner 
-#var area3D
-#var isInsideArea = false
-#var GameController
-#
-#func _ready():
-	##area3D = null
-	##GameController = get_node("GameController") 
-#func _on_body_entered(area):
-	##if area is Area3D:
-		##area3D = area  # Assign the entered Area3D to area3D
-		##isInsideArea = true
-		### Remove the RigidBody3D component
-		#if has_node("RigidBody3D"):
-			#var rigidbody = get_node("RigidBody3D")
-			#spawner.apply_impulse(ballspeed)
-			#remove_child(rigidbody)
-			#rigidbody.queue_free()
-#
-#func _process(_delta):
-	#if isInsideArea:
-		#if area3D:
-			#set_position(area3D.global_transform.origin)
+
 
 func _on_timer_timeout():
 	#print("KILL")
