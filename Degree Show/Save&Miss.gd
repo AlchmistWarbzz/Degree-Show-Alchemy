c3bbc60b967799086abf437010e8c73de8ae802e
AlchmistWarbzz <ALchemistwarbzz@gmail.com>
2024-05-27 13:32:12 +0100

    s

--- Degree Show/Save&Miss.gd
+++ Degree Show/Save&Miss.gd
@@ -12,12 +12,12 @@ func _ready():
 func _process(delta):
 	if Global.GK_save == true:
 		save.visible = true
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.3).timeout
 		Global.GK_save = false
 		save.visible = false
 	if Global.GK_miss == true:
 		miss.visible = true
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.3).timeout
 		Global.GK_miss = false
 		miss.visible = false
 		
