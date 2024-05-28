e1bdf10d47a4cfc4918a866165b112bd67fe09f2
AlchmistWarbzz <ALchemistwarbzz@gmail.com>
2024-05-28 19:16:54 +0100

    Score system

--- Degree Show/Game Content/Scripts/CheckBall.gd
+++ Degree Show/Game Content/Scripts/CheckBall.gd
@@ -6,7 +6,7 @@ var highest_score = 0
 var player_pos
 # Access the controller
 var input_controller = null
-
+var scored:bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Access the input controller
@@ -41,30 +41,18 @@ func checkball(body):
 		ball = body
 		#print("Entered ball")
 		Input.vibrate_handheld(500)
-		if body.name == "gravity":
-			Global.score += 1
-			Global.saves += 1
-			Global.time += 25
-			body.name = "balls"
-			Global.GK_save = true
-			Global.save_sound = true
-			await get_tree().create_timer(0.02).timeout
-			Global.save_sound = false
-		else:
-			Global.score += 1
-			Global.saves += 1
-			Global.time += 20
-			body.name = "balls"
-			Global.GK_save = true
-			Global.save_sound = true
-			await get_tree().create_timer(0.02).timeout
-			Global.save_sound = false
-			
-		var direction = global_transform.origin - ball.global_transform.origin
-		# Reflect the ball's velocity around the normal of the collision surface
-		var reflection = direction.normalized().reflect(ball.linear_velocity.normalized())
-		# Apply the reflected velocity to the ball
-		ball.set_linear_velocity(reflection * ball.linear_velocity.length())
+		if score == false:
+			if body.name == "gravity":
+				score_system(1, 1, 25)
+			elif body.name == "curve":
+				score_system(1, 1, 30)
+			elif body.name == "speed":
+				score_system(1, 1, 25)
+			elif body.name == "ball":
+				score_system(1, 1, 20)
+		scored = true
+		deflect()
+
 	# Trigger vibration
 		if input_controller:
 			input_controller.triggerHapticPulse(0, 0.5) # Adjust the intensity as needed
@@ -73,3 +61,18 @@ func checkball(body):
 		ball.queue_free()
 		print("hit")
 	
+func score_system(score, save, time):
+	Global.score += score
+	Global.saves += save
+	Global.time += time
+	Global.GK_save = true
+	Global.save_sound = true
+	await get_tree().create_timer(0.02).timeout
+	Global.save_sound = false
+
+func deflect():
+	var direction = global_transform.origin - ball.global_transform.origin
+# Reflect the ball's velocity around the normal of the collision surface
+	var reflection = direction.normalized().reflect(ball.linear_velocity.normalized())
+		# Apply the reflected velocity to the ball
+	ball.set_linear_velocity(reflection * ball.linear_velocity.length())
