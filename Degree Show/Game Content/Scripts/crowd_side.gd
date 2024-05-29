c1b7215a4a30ddca55b1e1d085abf9f482be71f0
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-05-29 14:45:30 +0100

    Update tween for individual crowd members
    add export variables and use randomness to differentiate individual crowd jump sequences

--- Degree Show/Game Content/Scripts/crowd_side.gd
+++ Degree Show/Game Content/Scripts/crowd_side.gd
@@ -1,16 +1,22 @@
 extends Node3D
 
+@export var min_jump_duration := 0.2
+@export var max_jump_duration := 0.4
+@export var idle_jump_height := 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var tween = create_tween()
-	tween.set_loops()
+	
 	for mesh: Node3D in get_children():
+		var tween = mesh.create_tween()
+		tween.set_loops()
 		var pos := mesh.position
-		tween.tween_property(mesh, "position", Vector3(pos.x, pos.y + 0.5, pos.z), 0.5)
-		tween.tween_property(mesh, "position", pos, 0.5)
+		var duration := randf_range(min_jump_duration, max_jump_duration)
+		tween.tween_property(mesh, "position", Vector3(pos.x, pos.y + idle_jump_height, pos.z), duration)
+		tween.tween_property(mesh, "position", pos, duration)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
